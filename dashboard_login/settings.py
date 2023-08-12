f5e973cbf523f9540abcbb2620b02d4521d981ae
munafsamufidi11 <munafsamufidi11@gmail.com>
2023-08-12 17:28:19 +0700

    edit 1

--- dashboard_login/settings.py
+++ dashboard_login/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^8ia)3+bs0ifpu0(zoc@+wct*i30c)si7gs-pmn$*#@+v6m490
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 # Application definition
 
