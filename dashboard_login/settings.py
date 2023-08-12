cc11a70ea35fa5c81e54ce3ae5eda1852e6b5158
msamufidi <munafsamufidi11@gmail.com>
2023-08-12 17:34:06 +0700

    Update settings.py

--- dashboard_login/settings.py
+++ dashboard_login/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^8ia)3+bs0ifpu0(zoc@+wct*i30c)si7gs-pmn$*#@+v6m490'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.vercel.app']
 
